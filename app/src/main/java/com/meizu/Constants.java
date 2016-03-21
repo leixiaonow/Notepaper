2bbadcfcc0334b1184bea4b13b77d50257952d63
leixiaonow <641703455@qq.com>
2016-03-21 14:55:50 +0800

    2016-3-21 阅读了NotPaperProvider，注释了部分代码，理了下函数的大概功能

---
+++ app/src/main/java/com/meizu/Constants.java
@@ -0,0 +1,39 @@
+package com.meizu;
+
+public class Constants {
+    public static final String HOST_U = "http://u.meizu.com";
+    public static final String JSON_KEY_CODE = "code";
+    public static final String JSON_KEY_DEVICE_TYPE = "deviceType";
+    public static final String JSON_KEY_EXISTS_UPDATE = "existsUpdate";
+    public static final String JSON_KEY_FW = "firmware";
+    public static final String JSON_KEY_IMEI = "imei";
+    public static final String JSON_KEY_LATEST_VERSION = "latestVersion";
+    public static final String JSON_KEY_NEED_UPDATE = "needUpdate";
+    public static final String JSON_KEY_RELEASE_DATE = "releaseDate";
+    public static final String JSON_KEY_RELEASE_NOTE = "releaseNote";
+    public static final String JSON_KEY_REPLY = "reply";
+    public static final String JSON_KEY_SERVICE = "services";
+    public static final String JSON_KEY_SERVICE_NAME = "serviceName";
+    public static final String JSON_KEY_SERVICE_TOKEN = "serviceToken";
+    public static final String JSON_KEY_SIZE = "fileSize";
+    public static final String JSON_KEY_SN = "sn";
+    public static final String JSON_KEY_SUB_STATUS = "subStatus";
+    public static final String JSON_KEY_SYSTEM_V = "sysVer";
+    public static final String JSON_KEY_TYPE = "type";
+    public static final String JSON_KEY_UPDATE_URL = "updateUrl";
+    public static final String JSON_KEY_VALUE = "value";
+    public static final String JSON_KEY_VERSION = "version";
+    public static final String PARAM_APPS = "apps";
+    public static final String PARAM_SIGN = "sign";
+    public static final String PARAM_SUB_SERVICE = "subservices";
+    public static final String PUSH_REGISTER = "http://u.meizu.com/subscription/registerWithSign";
+    public static final String REQUEST_MODEL_NAME = "All";
+    public static final String REQUEST_SYSTEM_VERSION = "All";
+    public static final int RESPONSE_CODE_OK = 200;
+    public static final int RESPONSE_CODE_PARTIAL = 206;
+    public static final int RESPONSE_CODE_SUCCESS = 200;
+    public static final String SIGN_STRING = "2635881a7ab0593849fe89e685fc56cd";
+    public static final String URL_CHECK_UPDATE = "http://u.meizu.com/appupgrade/check";
+    public static final String USER_AGENT_MEIZU = "MEIZU";
+    public static final String UTF_8_CODE = "UTF-8";
+}
